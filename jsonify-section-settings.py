22a2e7bb9385e69f16cca757c5ab9d927ad382ae
vbettsPxU <victoria.betts@pixelunion.net>
2022-04-27 14:20:20 -0700

    Add jsonify script

---
+++ jsonify-section-settings.py
@@ -0,0 +1,59 @@
+import csv
+import json
+
+
+settings_dict = {}
+with open("settings.csv", 'r') as settings:
+    data = csv.DictReader(settings, delimiter=",")
+    for row in data:
+        sectionId = row["section_name"]
+        blockId = row["block_name"]
+        settingId = row["setting_id"]
+        settingLevel = row["level"]
+        settingType = row["type"]
+        settingProperty = row["property"]
+        settingContent = row["content"]
+
+        if sectionId not in settings_dict:
+            headerCount = 0
+            settings_dict[sectionId] = {}
+
+        if settingLevel == "sections":
+            if settingProperty == "section name":
+                settings_dict[sectionId]["name"] = settingContent
+                continue
+            else:
+                if settingType == "header":
+                    headerCount += 1
+                    settingId = "header_"+str(headerCount)
+
+                if settingId not in settings_dict[sectionId]:
+                    settings_dict[sectionId][settingId] = {}
+
+                settings_dict[sectionId][settingId][settingProperty] = settingContent
+        elif settingLevel == "blocks":
+            if "blocks" not in settings_dict[sectionId]:
+                blockHeaderCount = 0
+                settings_dict[sectionId]["blocks"] = {}
+
+            if settingProperty == "block name":
+                settings_dict[sectionId]["name"] = settingContent
+                continue
+            else:
+                if settingType == "header":
+                    blockHeaderCount += 1
+                    settingId = "header_"+str(blockHeaderCount)
+
+                if settingId not in settings_dict[sectionId]["blocks"]:
+                    settings_dict[sectionId]["blocks"][settingId] = {}
+
+                settings_dict[sectionId]["blocks"][settingId][settingProperty] = settingContent
+
+        elif settingLevel == "presets":
+            if "presets" not in settings_dict[sectionId]:
+                settings_dict[sectionId]["presets"] = {}
+
+            settings_dict[sectionId]["presets"][settingProperty] = settingContent
+
+with open("sample.json", "w") as outfile:
+    json.dump(settings_dict, outfile, indent=2 )
--- parse_schema.py
+++ parse_schema.py
@@ -8,32 +8,34 @@ def processSettings(settings, name):
     for setting in settings:
         level = "settings_schema"
         settingType = setting["type"]
+        sectionName = name
         if "id" in setting:
             settingID = setting["id"]
             settingLabel = setting["label"]
             settingProperty = "label"
-            line = [file, settingID, level, settingType, settingProperty, settingLabel]
+            line = [file, settingID, level, sectionName, "", settingType, settingProperty, settingLabel]
             data.append(line)
 
             if "info" in setting:
                 settingProperty = "info"
                 settingInfo = setting["info"]
-                line = [file, settingID, level, settingType, settingProperty, settingInfo]
+                line = [file, settingID, level, sectionName, "", settingType, settingProperty, settingInfo]
                 data.append(line)
 
             if setting["type"] == "select":
                 options = setting["options"]
+                count = 1
                 for option in options:
-                    settingID = setting["id"]+"["+option["value"]+"]"
-                    settingProperty = "option label"
+                    settingProperty = "option_" + str(count)
                     settingLabel = option["label"]
-                    line = [file, settingID, level, settingType, settingProperty, settingLabel]
+                    line = [file, settingID, level, sectionName, "", settingType, settingProperty, settingLabel]
                     data.append(line)
+                    count+=1
         else:
             settingID = setting["type"]
             settingLabel = setting["content"]
             settingProperty = "content"
-            line = [file, settingID, level, settingType, settingProperty, settingLabel]
+            line = [file, settingID, level, sectionName, "", settingType, settingProperty, settingLabel]
             data.append(line)
     return data
 
@@ -41,7 +43,7 @@ directory = "./"
 pathname = directory + "settings_schema.json"
 files = glob.glob(pathname, recursive=True)
 
-header = ['filename', 'Setting ID', 'Section/Block', 'Type', 'Property', 'Content']
+header = ['filename', 'setting_id', 'level', 'section_name', 'block_name', 'type', 'property', 'content']
 dataList = []
 
 for file in files:
@@ -49,10 +51,10 @@ for file in files:
     jsonData = json.load(f)
     for group in jsonData:
         if "settings" in group:
-            dataList.append([group["name"], "", "", "", "", ""])
-            dataList.append([file, "name", "settings_schema", "group name", "name", group["name"]])
+            formattedGroupName = group["name"].replace(" ", "_").lower()
+            dataList.append([file, "name", "settings_schema", formattedGroupName , "", "", "name", group["name"]])
 
-            processedSettings = processSettings(group["settings"], group["name"])
+            processedSettings = processSettings(group["settings"], formattedGroupName)
             for line in processedSettings:
                 dataList.append(line)
 
