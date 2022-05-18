56f2d4d09079a155331b3a409b95147b6ca37548
vbettsPxU <victoria.betts@pixelunion.net>
2022-05-18 16:14:56 -0700

    Convert script to JS

---
+++ translations.js
@@ -0,0 +1,197 @@
+const glob = require('glob');
+const fs = require('fs');
+
+const translateableProps = [
+  'info',
+  'content',
+  'label',
+  'name',
+  'group',
+  'placeholder',
+  'category',
+];
+
+const enSchema = {
+  sections: {},
+  settings_schema: {},
+};
+
+
+ const sectionSettingsFiles = glob.sync('./source/sections/**/*.json');
+// const sectionSettingsFiles = glob.sync('./source/sections/dynamic-featured-product/dynamic-featured-product.json');
+const settingsSchemaFile = './source/config/settings_schema.json';
+
+
+function processSettings(content, translationPath, sectionKey, blockName = null) {
+  let headerCount = 0;
+  let paragraphCount = 0;
+
+  const updatedSettings = content.map(setting => {
+    let settingId;
+    if (setting.id) {
+      settingId = setting.id;
+    } else if (setting.type === 'header') {
+      // if the setting does not have an ID, it is a header or a paragraph setting
+      headerCount += 1;
+      settingId = `header_${headerCount}`;
+    } else if (setting.type === 'paragraph') {
+      paragraphCount += 1;
+      settingId = `paragraph_${paragraphCount}`;
+    }
+
+    let localSchema = enSchema.sections;
+
+    if (translationPath.includes('settings_schema')) {
+      localSchema = enSchema.settings_schema;
+    }
+
+    if (blockName) {
+      localSchema[sectionKey].blocks[blockName][settingId] = {};
+    } else {
+      localSchema[sectionKey][settingId] = {};
+    }
+
+    Object.keys(setting).forEach(settingProp => {
+      if (translateableProps.includes(settingProp)) {
+        const settingValue = setting[settingProp];
+        if (blockName) {
+          localSchema[sectionKey].blocks[blockName][settingId][settingProp] = settingValue;
+        } else {
+          localSchema[sectionKey][settingId][settingProp] = settingValue;
+        }
+        setting[settingProp] = `${translationPath}.${settingId}.${settingProp}`;
+      }
+    });
+
+    if (setting.options) {
+      let optionCount = 0;
+      setting.options.forEach(option => {
+        Object.keys(option).forEach(settingProp => {
+          if (translateableProps.includes(settingProp)) {
+            const settingValue = option[settingProp];
+            optionCount += 1;
+            const optionSettingProp = `option_${optionCount}`;
+            if (blockName) {
+              localSchema[sectionKey].blocks[blockName][settingId][optionSettingProp] = settingValue;
+            } else {
+              localSchema[sectionKey][settingId][optionSettingProp] = settingValue;
+            }
+
+            option[settingProp] = `${translationPath}.${settingId}.${optionSettingProp}`;
+          }
+        });
+      });
+    }
+
+
+    if (translationPath.includes('settings_schema')) {
+      enSchema.settings_schema = localSchema;
+    } else {
+      enSchema.sections = localSchema;
+    }
+
+    return setting;
+  });
+
+  return updatedSettings;
+}
+
+sectionSettingsFiles.forEach(filePath => {
+  const fileContent = JSON.parse(fs.readFileSync(filePath));
+
+  const sectionKey = fileContent.name.replace(' ', '_').toLowerCase();
+
+  enSchema.sections[sectionKey] = {
+    name: fileContent.name,
+  };
+
+  const translationPath = `t:sections.${sectionKey}`;
+  fileContent.name = `${translationPath}.name`;
+
+  if (fileContent.settings.length) {
+    fileContent.settings = processSettings(fileContent.settings, translationPath, sectionKey, null);
+
+    // TODO: Account for blocks, presets
+    if (fileContent.blocks) {
+      enSchema.sections[sectionKey].blocks = {};
+
+      const tempBlocks = fileContent.blocks;
+
+      const blockTranslationPath = `${translationPath}.blocks`
+
+      const updatedBlocks = tempBlocks.map(block => {
+        if (block.name) {
+          const blockName = block.name.replace(' ', '_').toLowerCase();
+          enSchema.sections[sectionKey].blocks[blockName] = {
+            name: block.name
+          };
+          block.name = `${blockTranslationPath}.${blockName}.name`;
+          if (block.settings) {
+            const blockSettingTranslationPath = `${blockTranslationPath}.${blockName}`;
+            const blockSettings = processSettings(block.settings, blockSettingTranslationPath, sectionKey, blockName);
+            block.settings = blockSettings;
+          }
+        }
+        return block;
+      })
+
+      fileContent.blocks = updatedBlocks;
+    }
+
+    if (fileContent.presets) {
+      const presets = fileContent.presets;
+
+      enSchema.sections[sectionKey].presets = {};
+
+      updatedPresets = presets.map(preset => {
+        const presetName = preset.name.replace(' ', '_').toLowerCase();
+        enSchema.sections[sectionKey].presets[presetName] = {
+          name: preset.name,
+          category: preset.category,
+        };
+
+        preset.name = `${translationPath}.presets.${presetName}.name`;
+        preset.category = `${translationPath}.presets.${presetName}.category`;
+
+        return preset;
+      });
+
+      fileContent.presets = updatedPresets;
+    }
+  }
+
+  const fileContentString = JSON.stringify(fileContent, null, 2);
+
+  fs.writeFileSync(filePath, fileContentString);
+});
+
+// TODO: config/settings_schema
+
+fs.readFileSync(settingsSchemaFile)
+
+const configFileContent = JSON.parse(fs.readFileSync(settingsSchemaFile));
+
+const updatedConfigFile = configFileContent.map(group => {
+  if (group.settings) {
+    const groupKey = group.name.replace(' ', '_').toLowerCase();
+    const schemaTranslationPath =`t:settings_schema.${groupKey}`;
+
+    enSchema.settings_schema[groupKey] = {
+      name: group.name
+    };
+
+    const updatedSettings = processSettings(group.settings, schemaTranslationPath, groupKey);
+
+    group.settings = updatedSettings;
+  }
+  return group;
+});
+
+const configFileString = JSON.stringify(updatedConfigFile, null, 2);
+
+fs.writeFileSync(settingsSchemaFile, configFileString);
+
+
+const schemaString = JSON.stringify(enSchema, null, 2);
+
+fs.writeFileSync('./source/locales/en.default.schema.json', schemaString);
